a70554ddb35e3dab7a1ea11601d6f0efbf184fb6
Amanda de Oliveira <amanda.londrina.pr@gmail.com>
2026-03-21 00:19:11 -0300

    Refactor: Remove pydantic from service

--- app/models/task_model.py
+++ app/models/task_model.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from tortoise import Model, fields
 
 
@@ -9,19 +7,3 @@ class Task(Model):
     title = fields.CharField(max_length=200)
     description = fields.TextField(null=True)
     completed = fields.BooleanField(default=False)
-
-
-class TaskPage:
-    def __init__(
-            self,
-            data: List[Task],
-            total: int,
-            page: int,
-            page_size: int,
-            total_pages: int,
-    ):
-        self.data = data
-        self.total = total
-        self.page = page
-        self.page_size = page_size
-        self.total_pages = total_pages
